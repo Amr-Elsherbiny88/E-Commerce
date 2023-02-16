57c4ec553013ea878c39cda2b18baa4c72ca1b59
Amr-Elsherbiny88 <121190232+Amr-Elsherbiny88@users.noreply.github.com>
2023-02-16 21:51:58 +0200

    Add files via upload

--- Contact.js
+++ Contact.js
@@ -55,8 +55,36 @@ toggle.addEventListener('click', function(){
 
 
 // email js
-
 function sendMail() {
+
+  var from_name =  document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var error = document.getElementById("error");
+  var errror = "";
+
+  if (from_name.length ==""){
+    errror = "please enter your name"
+    error.innerHTML= errror ;
+    return false; 
+  }
+
+
+  else if (email.indexOf("@") == -1 || email.length < 10){
+    errror = "please enter Valid Email"
+    error.innerHTML= errror ;
+    return false; 
+  }
+
+
+  else if (message.length ==""){
+    errror = "please enter a message"
+    error.innerHTML= errror ;
+    return false; 
+    
+  }
+  
+  else {
     var params = {
       from_name: document.getElementById("name").value,
       email: document.getElementById("email").value,
@@ -77,5 +105,10 @@ function sendMail() {
       })
       .catch(err=>console.log(err));
   
+    return true;
   }
+  
 
+
+  
+}
